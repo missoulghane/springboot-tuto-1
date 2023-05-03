605952c9c493973c528d74e2f19aaca0344912d5
missoulghane <m.issoulghane@gmail.com>
2023-05-03 17:19:55 +0200

    first commit springboot

---
+++ src/main/java/com/example/demo/SpringbootEx1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx1Application {
+
+	// Demo
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEx1Application.class, args);
+	}
+
+}
